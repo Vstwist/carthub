10f342726b5756abb9c9f94d1733bd2fa101f5fa
Hitesh <hiteshchintu8@gmail.com>
2026-07-12 03:34:00 -0700

    Fix Login.js syntax error

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -10,7 +10,7 @@ function Login() {
   const handleLogin = async () => {
     try {
        const response = await axios.post(
-  `${process.env.REACT_APP_API_URL}/api/users/login`
+  `${process.env.REACT_APP_API_URL}/api/users/login`,
   {
     email,
     password,
